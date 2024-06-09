df37b9433911a9748605753bb684ed22a4f4366a
Vinoth <rajiivini@gmail.com>
2024-06-09 16:10:41 +0530

    vinoth

---
+++ src/test/java/Browsernavigation/Createworktypegroup.java
@@ -0,0 +1,15 @@
+package Browsernavigation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Createworktypegroup {
+
+	public static void main(String[] args) {
+		//webDriverManager 
+		//webDriverManager driver =new ChromeDriver();
+		ChromeDriver driver =new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
